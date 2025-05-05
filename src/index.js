3f30aa68cd1e6df204dc1a326bebfec167ffde16
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-05-05 22:02:53 +0530

    connect to mango db

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import express from "express";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+const app = express();
+
+connectDb();
+
+// async () => {
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//       app.on("error", () => {
+//         console.log("ERROR", error);
+//         throw error;
+//       });
+
+//       app.listen(process.env.PORT, () => {
+//         console.log(`app is listening on port ${process.env.PORT}`);
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
